01a4ba6118e5d28b5c3433017ceb99d41447de55
Julieta <izzisitas@gmail.com>
2026-05-27 10:08:37 -0300

    arrays java script

---
+++ javascriptarray/jvas.js
@@ -0,0 +1,15 @@
+const precios = [100,200,300];
+
+const conIva = precios.map(precio => {return precio * 1.21});
+
+
+console.log(precios);
+console.log(conIva);
+
+const precio = [100,200,300];
+
+const siniva = precios.map(precio => {return precio / 0,82});
+
+
+console.log(precios);
+console.log(siniva);
